2f8cacc07ae1aae08dafe43b2214bac123f5e91e
Sheraaz Khan <sheraaz30@gmail.com>
2022-06-01 06:32:33 -0400

    first commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+(function (){
+
+    function Start()
+    {
+        console.log ("app started")
+    }
+    window.addEventListener ("load", Start)
+
+})();
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', heading: 'My Profile',
+  para: '' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', heading: 'My Profile',
+                        para: ''});
+});
+/* GET About us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About', heading: '',
+   para: 'My name is Sheraaz Khan and I have studied physio and Massage Therapy. I am a person who enjoys physical activities. I was born in the Caribbean but grew up in Canada.I have tried my best to take advantage of the seasons in Canada. I enjoy Snowboarding in winter and Hiking in the summer.'
+});
+});
+
+/* GET Products page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects', heading: 'Some of my projects',
+   para: '' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services', heading: 'Services' ,
+  para: 'I do not offer Services.'});
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact us', heading: 'Contact',
+  para: 'you can reach me by cell or email' });
+});
+
+
+
+module.exports = router;
