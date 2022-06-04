c0b0ab4809298d60213fff059ce21c3efe3be11f
Sheraaz Khan <sheraaz30@gmail.com>
2022-06-03 21:30:03 -0400

    6th commit

--- routes/index.js
+++ routes/index.js
@@ -9,32 +9,26 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home', heading: 'My Profile',
-                        para: ''});
+  res.render('index', { title: 'Home'});
 });
 /* GET About us page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About', heading: '',
-   para: 'My name is Sheraaz Khan and I have studied physio and Massage Therapy. I am a person who enjoys physical activities. I was born in the Caribbean but grew up in Canada.I have tried my best to take advantage of the seasons in Canada. I enjoy Snowboarding in winter and Hiking in the summer.'
-});
+  res.render('about', { title: 'About'});
 });
 
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects', heading: 'Some of my projects',
-   para: '' });
+  res.render('projects', { title: 'Projects'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services', heading: 'Services' ,
-  para: 'I do not offer Services.'});
+  res.render('services', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact us', heading: 'Contact',
-  para: 'you can reach me by cell or email' });
+  res.render('contact', { title: 'Contact us'});
 });
 
 
